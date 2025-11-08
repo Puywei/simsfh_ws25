407d18425ab23bc6025a1decdcffadc7b02332df
Margarethe Puschnig <is241318@fhstp.ac.at>
2025-11-08 14:31:06 +0100

    test

--- sims-api/Program.cs
+++ sims-api/Program.cs
@@ -16,7 +16,7 @@ public class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+        // Configure the HTTP request pipeline. TEST
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
