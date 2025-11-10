04b4ee2f9e831ca052d6157ec23dfc45c01cec5a
Philipp <is241312@fhstp.ac.at>
2025-11-10 19:09:06 +0100

    Implemented basic User-API against Keycloak. Created Dockercompose and Dockerfile for project

---
+++ sims-api/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    // Public endpoint
+    [HttpGet("public")]
+    public IActionResult PublicEndpoint()
+    {
+        return Ok("This is a public endpoint.");
+    }
+
+    // Secure endpoint (requires Keycloak JWT)
+    [Authorize]
+    [HttpGet("secure")]
+    public IActionResult SecureEndpoint()
+    {
+        return Ok("This is a secure endpoint, only accessible with a valid Keycloak token.");
+    }
+}
