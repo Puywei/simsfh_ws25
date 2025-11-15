e1071f0777d9d3c1109263ebd833ed4b4acb4c6e
puywei <puywei@proton.me>
2025-11-15 19:56:58 +0100

    add WebApp Api Service

---
+++ sims-web_app/Data/Model/IncidentComment.cs
@@ -0,0 +1,15 @@
+﻿namespace sims_web_app.Data.Model;
+
+public class IncidentComment
+{
+    public Guid CommentId { get; set; }
+    
+    public int CommentOrder { get; internal set; }
+    
+    public Incident Incident { get; set; }
+    public string IncidentId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CreateDate { get; set; }
+    public string? UserName { get; set; }
+    public string? Comment { get; set; }
+}
