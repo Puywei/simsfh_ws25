ec330932ec722bed080ee08b55a2a390b8be4d66
Konatsu Macala <is241305@fhstp.ac.at>
2025-11-15 23:54:24 +0100

    Add_User, Add_Customer & Model .cs

---
+++ sims-web_app/Data/Model/CustomerOverview.cs
@@ -0,0 +1,9 @@
+﻿using sims_web_app.Data.Model.Enum;
+
+namespace sims_web_app.Data.Model
+{
+    public class CustomerOverview : Customer
+    {
+        public bool selected { get; set; }
+    }
+}
