af8f5058ee48b5a01daa322bf8fd517e123d0dcf
puywei <puywei@proton.me>
2025-11-19 10:04:17 +0100

    added Login function

---
+++ sims-web_app/Components/Identity/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿
+using Newtonsoft.Json;
+using sims_web_app.Components.Identity.Contracts;
+
+namespace sims_web_app.Components.Identity.Services;
+
+public class AuthService
+{
+    private readonly HttpClient _httpClient;
+
+    public AuthService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<AuthResponse?> Login(AuthRequest request)
+    {
+        var result = await _httpClient.PostAsJsonAsync("auth/login", request);
+        if (result.IsSuccessStatusCode)
+        {
+            var content = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<AuthResponse>(content);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public async Task<bool> Register(RegisterRequest request)
+    {
+        var result = await _httpClient.PostAsJsonAsync("auth/register", request);
+        return result.IsSuccessStatusCode;
+    }
+}
--- sims-web_app/Program.cs
+++ sims-web_app/Program.cs
@@ -2,6 +2,9 @@ using MudBlazor.Services;
 using sims_web_app.Components;
 using sims_web_app.Services;
 using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using sims_web_app.Components.Identity.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMudServices();
 
 //Add Blazored LocalStorage
+builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddBlazoredLocalStorage();
+//Add AuthService
+builder.Services.AddHttpClient<AuthService>(options =>
+{
+    options.BaseAddress = new Uri("http://localhost:5000/api/");
+});
+builder.Services.AddAuthentication("Identity.Application") // beliebiger Scheme-Name
+    .AddCookie("Identity.Application"); // Cookie-Auth wird benötigt, sonst meckert IAuthenticationService
+
+builder.Services.AddAuthorizationCore(); // für [Authorize] in Blazor
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
