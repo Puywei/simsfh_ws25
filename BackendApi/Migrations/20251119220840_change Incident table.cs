82b6b4a65024a07ad61e9844f8601a2cf138431e
Maximilian Radler <maximilian.radler@gmail.com>
2025-11-19 23:22:06 +0100

    db migrations BackenApi

---
+++ BackendApi/Migrations/20251119220840_change Incident table.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeIncidenttable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignedPerson",
+                table: "Incidents",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedPersonName",
+                table: "Incidents",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UUId",
+                table: "Customers",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedPersonName",
+                table: "Incidents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AssignedPerson",
+                table: "Incidents",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UUId",
+                table: "Customers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
