8e1c5c57d8228106df315ad1b539db1aa919e986
Philipp <is241312@fhstp.ac.at>
2025-11-21 20:43:16 +0100

    Added some Logging for retrieving Users and Roles API, Attempting to authorize Endpoints with invalid token, blacklisted token

--- sims-api/Controllers/UserController.cs
+++ sims-api/Controllers/UserController.cs
@@ -160,17 +160,28 @@ namespace sims.Controllers
                  [Authorize]
                  public IActionResult GetMe()
                  {
+                     var actingUser = UserContextHelper.GetActingUserInfo(User);
+                     
                      var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
                      var email = User.Identity?.Name;
                      var role = User.FindFirst(ClaimTypes.Role)?.Value;
+
+                     _eventLogger.LogEventAsync(
+                         $"Acting User:{actingUser.Email} - Requested current user token info",
+                         severity: 1
+                     );
          
                      return Ok(new { userId, email, role });
+                     
+                     
                  }
         //  Get all users endpoint
         [HttpGet("getAllUsers")]
         [Authorize]
         public async Task<IActionResult> GetAllUsers()
         {
+            var actingUser = UserContextHelper.GetActingUserInfo(User);
+            
             var users = await _db.Users
                 .Include(u => u.Role)
                 .Select(u => new
@@ -182,6 +193,11 @@ namespace sims.Controllers
                     role = u.Role.RoleName
                 })
                 .ToListAsync();
+            
+            await _eventLogger.LogEventAsync(
+                $"Acting User:{actingUser.Email} - Requested All Users list",
+                severity: 3
+            );
 
             return Ok(users);
         }
@@ -190,6 +206,8 @@ namespace sims.Controllers
         [Authorize]
         public async Task<IActionResult> GetAllRoles()
         {
+            var actingUser = UserContextHelper.GetActingUserInfo(User);
+            
             var roles = await _db.Roles
                 .Select(r => new
                 {
@@ -197,6 +215,11 @@ namespace sims.Controllers
                     RoleName = r.RoleName
                 })
                 .ToListAsync();
+            
+            await _eventLogger.LogEventAsync(
+                $"Acting User:{actingUser.Email} - Listed all Roles",
+                severity: 3
+            );
 
             return Ok(roles);
         }
